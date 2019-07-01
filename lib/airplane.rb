04f82e3d0343999a59c36e1f86093cf5592d0524
iAquino47 <ianaquino47@gmail.com>
2019-07-01 23:16:37 +0100

    first commit

---
+++ lib/airplane.rb
@@ -0,0 +1,24 @@
+require 'airport'
+require 'weather'
+
+class AirPlane
+  include Weather
+  def initialize
+    @inair = false
+  end
+  attr_reader :inair
+
+  def land(airport)
+    return "cannot land because plane not in air" unless @inair
+    return "can only land at airport" unless airport.instance_of?(AirPort)
+    return "stormy weather, cannot land" if stormy?
+    return "airport full, cannot land" if airport.full?
+
+    @inair = false
+    airport.dock(self)
+  end
+
+  def goes_in_air
+    @inair = true
+  end
+end
